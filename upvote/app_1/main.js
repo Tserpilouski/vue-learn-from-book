7b6ed74a7edafbf265e174e31ee48ecc894cab58
Kiryl Tserpilouski <4ern14ka@404.local>
2023-11-12 13:45:58 +0100

    changed vanila to a component object

--- upvote/app_1/main.js
+++ upvote/app_1/main.js
@@ -1,4 +1,44 @@
-const submissionComponent = {};
+const submissionComponent = {
+  template: `<div style="display: flex; width: 100%">
+    <figure class="media-left">
+      <img class="image is-64x64"
+        v-bind:src="submission.submissionImage">
+    </figure>
+    <div class="media-content">
+      <div class="content">
+<p> <strong>
+            <a v-bind:href="submission.url" class="has-text-info">
+              {{ submission.title }}
+</a>
+            <span class="tag is-small">#{{ submission.id }}</span>
+          </strong>
+          <br>
+            {{ submission.description }}
+          <br>
+          <small class="is-size-7">
+            Submitted by:
+            <img class="image is-24x24"
+              v-bind:src="submission.avatar">
+          </small>
+</p> </div>
+    </div>
+    <div class="media-right">
+    <span class="icon is-small" v-on:click="upvote(submission.id)">
+        <i class="fa fa-chevron-up"></i>
+        <strong class="has-text-info">{{ submission.votes }}</strong>
+      </span>
+    </div>
+</div>`,
+  props: ["submission", "submissions"],
+  methods: {
+    upvote(submissionID) {
+      const submission = this.submissions.find(
+        (submission) => submission.id === submissionID
+      );
+      submission.votes++;
+    },
+  },
+};
 
 const upvoteApp = {
   data() {
@@ -13,13 +53,8 @@ const upvoteApp = {
       });
     },
   },
-  methods: {
-    upvote(submissionID) {
-      const submission = this.submissions.find(
-        (submission) => submission.id === submissionID
-      );
-      submission.votes++;
-    },
+  components: {
+    "submission-component": submissionComponent,
   },
 };
 
